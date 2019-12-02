123c7ddd2a70b3173010b30f56345866d44d31ae
jdrenteria <jesusrenteria48@gmail.com>
2019-12-01 20:22:58 -0600

    Added all necessary folders and files to start coding. Added app, data, and routing folders with their files necesarry.As well as server code, friends.js examples with picutres.Finally started added home.html code

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+        name: "Son Goku",
+        photo:
+        "app\data\example-pic\goku.jpg",
+        scores:[5,1,4,4,5,1,2,5,4,4]
+    },
+    {
+        name: "Saitama",
+        photo:
+        "app\data\example-pic\saitama.jpg",
+        scores:[1,2,5,3,2,4,1,2,3,2]
+    },
+    {
+        name: "Hinata Hyuga",
+        photo:
+        "app\data\example-pic\hinata.jpg",
+        scores:[3,3,3,3,4,5,2,1,2,5]
+    },
+    {
+        name: "Eren Yeager",
+        photo:
+        "app\data\example-pic\eren.jpg",
+        scores:[3,3,2,4,5,1,2,4,5,3]
+    },
+    {
+        name: "Asuna Yuuki",
+        photo:
+        "app\data\example-pic\asuna.jpg",
+        scores:[5,4,2,3,5,4,5,4,4,4]
+    },
+    {
+        name: "Touka Kirishima",
+        photo:
+        "app\data\example-pic\touka.jpg",
+        scores:[5,5,5,5,4,2,4,5,5,2]
+    }
+];
+
+module.exports =friends;
