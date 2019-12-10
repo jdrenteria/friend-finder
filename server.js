b1f7fb6b2efa6c7fbe3ea0acf404bf0f882cb73b
jdrenteria <jesusrenteria48@gmail.com>
2019-12-10 17:37:38 -0600

    final touches on app, including a modal and on click function for submit button

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+
 var path = require( 'path');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:"aplication/vnd.api+json"}));
 
-app.use(express.static("app/public"));
-
-
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
 
 app.listen(port, () => console.log ('App is running on port %s',port));
