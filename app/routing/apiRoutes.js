d93e355a7a5f01263c97452546d6a4c2743aecfe
jdrenteria <jesusrenteria48@gmail.com>
2019-12-10 00:04:20 -0600

    added variables for user information on app

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,5 +3,26 @@ var friends = require("../data/friends");
 module.exports = function (app){
     app.get("/api/friends", function(req, res){
         res.json(friends);
-    })
-}
+    });
+
+    app.post("/api/friends", function(req,res){
+        var totalDifference = 0;
+        var bestMatch ={
+            name="",
+            photo="",
+            friendDifference:1000
+        };
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+
+        var b = userScores.map(function(item){
+            return parseInt(item, 10);
+        });
+        userData ={
+            name: req.body.name,
+            photo: req.body.photo,
+            scores = b
+        }
+    });
+};
