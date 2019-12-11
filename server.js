981aa12fb12b0ea01a087222f3b276b70f337b5b
jdrenteria <jesusrenteria48@gmail.com>
2019-12-10 19:05:28 -0600

    final changes

--- server.js
+++ server.js
@@ -6,5 +6,24 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 
+// EXPRESS DATA PARSING CAPABILITIES
+app.use(express.static('app/public'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.json());
+// app.use(express.static(path.resolve(__dirname, "app/public")));
+ // ================================================================================
+// ROUTER
+// The below points our server to a series of “route” files.
+// These routes give our server a “map” of how to respond when users visit or request data from various URLs.
+// ================================================================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 
 app.listen(port, () => console.log ('App is running on port %s',port));
