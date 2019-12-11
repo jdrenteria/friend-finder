f5eac4e3ce3b51cc142bac043db6cd95286d8518
jdrenteria <jesusrenteria48@gmail.com>
2019-12-10 20:30:39 -0600

    fixed syntax errors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,60 +1,66 @@
+
+
 var friends = require("../data/friends");
 
-module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
-        res.json(friends);
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    var totalDifference = 0;
+
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
+
+    var b = userScores.map(function(item) {
+      return parseInt(item, 10);
     });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: b
+    };
 
-    app.post("/api/friends", function (req, res) {
-        var totalDifference = 0;
-        var bestMatch = {
-            name="",
-            photo="",
-            friendDifference: 1000
-        };
-        var userData = req.body;
-        var userName = userData.name;
-        var userScores = userData.scores;
+    console.log("Name: " + userName);
+    console.log("User Score " + userScores);
 
-        var b = userScores.map(function (item) {
-            return parseInt(item, 10);
-        });
-        userData = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores = b
-        };
+    var sum = b.reduce((a, b) => a + b, 0);
 
-        console.log("Name:" + userName);
-        console.log("User score" + userScores);
+    console.log("Sum of users score " + sum);
+    console.log("Best match friend diff " + bestMatch.friendDifference);
+    console.log("+++++++=================++++++++++");
 
-        var sum = b.reduce((a, b) => a + b, 0);
-        console.log("Sum of users score" + sum);
-        console.log("Best match friend diff" + bestMatch.friendDifference);
-        console.log("++++++++++==============================================");
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+      totalDifference = 0;
+      console.log("Total Diff " + totalDifference);
+      console.log("Best match friend diff " + bestMatch.friendDifference);
 
-        for (var i = 0; i < friends.length; i++) {
-            console.log(firends[i].name);
-            totalDifference = 0;
-            console.log("Total Diff" + totalDifference);
-            console.log("Best match friends diff" + bestMatch.friendDifference);
+      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+      console.log("Total friend score " + bfriendScore);
+      totalDifference += Math.abs(sum - bfriendScore);
+      console.log("-------------------------> " + totalDifference);
 
-            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
-            console.log("Total friends score" + bfriendScore);
-            totalDifference += Math.abs(SUM - bfriendScore);
-            console.log("-------------------------->" + totalDifference);
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+      console.log(totalDifference + " Total Difference");
+    }
+    console.log(bestMatch);
 
-            if (totalDifference <= bestMatch, friendDifference) {
-                bestMatch.name = friends[i].name;
-                bestMatch.photo = friends[i].photo;
-                bestMatch.friendDifference = totalDifference;
-            }
-            console.log(totalDifference + "Total Difference");
-        }
-        console.log(bestMatch);
-        friends.push(userData);
-        console.log("New User Added");
-        console.log(userData);
-        res.json(bestMatch);
-    });
+    friends.push(userData);
+    console.log("New user added");
+    console.log(userData);
+    res.json(bestMatch);
+  });
 };
